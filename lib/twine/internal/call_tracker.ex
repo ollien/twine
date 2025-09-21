3fce9f978c1a213563b46ce276d29e89aae9b688
Nick Krichevsky <nick@ollien.com>
2025-09-21 19:38:27 -0400

    Refactor call_tracker to genserver

--- lib/twine/internal/call_tracker.ex
+++ lib/twine/internal/call_tracker.ex
@@ -3,6 +3,12 @@ defmodule Twine.Internal.CallTracker do
   # This module tracks calls for the Internal module. There is absolutely no
   # guarantee around its stability
 
+  defmodule State do
+    @moduledoc false
+
+    defstruct tracked_pids: %{}
+  end
+
   defmodule Entry do
     @moduledoc false
 
@@ -17,10 +23,10 @@ defmodule Twine.Internal.CallTracker do
     defstruct [:status, warnings: []]
   end
 
-  use Agent
+  use GenServer
 
   def start_link() do
-    Agent.start_link(fn -> %{} end)
+    GenServer.start_link(__MODULE__, nil)
   end
 
   @doc """
@@ -33,78 +39,99 @@ defmodule Twine.Internal.CallTracker do
 
     Any :ok result might contain a set of warnings
   """
-  def handle_event(tracker, {:trace, pid, :call, {_module, _function, _args} = mfa}) do
-    old_entry =
-      Agent.get_and_update(tracker, fn %{} = state ->
-        old_entry = Map.get(state, pid)
+  def handle_event(tracker, event) do
+    GenServer.call(tracker, {:handle_event, event})
+  end
 
-        state =
-          Map.put(
-            state,
-            pid,
-            %Entry{mfa: mfa, events: %{}}
-          )
+  @impl GenServer
+  def init(nil) do
+    {:ok, %State{}}
+  end
 
-        {old_entry, state}
-      end)
+  @impl GenServer
+  def handle_call(
+        {:handle_event, {:trace, pid, :call, {_module, _function, _args} = mfa}},
+        _from,
+        %State{} = state
+      ) do
+    old_entry = Map.get(state.tracked_pids, pid)
+
+    state = put_in(state.tracked_pids[pid], %Entry{mfa: mfa, events: %{}})
 
     case old_entry do
       nil ->
-        {:ok, %Result{status: :not_ready}}
+        {:reply, {:ok, %Result{status: :not_ready}}, state}
 
       %Entry{} ->
         warning = {:overwrote_call, pid, old_entry.mfa}
 
-        {:ok, %Result{status: :not_ready, warnings: [warning]}}
+        {:reply, {:ok, %Result{status: :not_ready, warnings: [warning]}}, state}
     end
   end
 
-  def handle_event(
-        tracker,
-        {:trace, pid, :return_from, {_module, _function, _arg_count} = mfa, return}
+  @impl GenServer
+  def handle_call(
+        {:handle_event,
+         {:trace, pid, :return_from, {_module, _function, _arg_count} = mfa, return}},
+        _from,
+        %State{} = state
       ) do
-    log_event(tracker, pid, normalize_mfa(mfa), :return_from, return)
+    {reply, state} = log_event(state, pid, normalize_mfa(mfa), :return_from, return)
+
+    {:reply, reply, state}
   end
 
-  def handle_event(
-        tracker,
-        {:trace, pid, :exception_from, {_module, _function, _arg_count} = mfa, error}
+  @impl GenServer
+  def handle_call(
+        {:handle_event,
+         {:trace, pid, :exception_from, {_module, _function, _arg_count} = mfa, error}},
+        _from,
+        %State{} = state
       ) do
-    log_event(tracker, pid, normalize_mfa(mfa), :exception_from, error)
+    {reply, state} = log_event(state, pid, normalize_mfa(mfa), :exception_from, error)
+
+    {:reply, reply, state}
   end
 
-  def handle_event(
-        tracker,
-        {:trace, pid, :return_to, {_module, _function, _arg_count} = mfa}
+  @impl GenServer
+  def handle_call(
+        {:handle_event, {:trace, pid, :return_to, {_module, _function, _arg_count} = mfa}},
+        _from,
+        %{} = state
       ) do
-    log_event(tracker, pid, :return_to, mfa)
+    {reply, state} = log_event(state, pid, :return_to, mfa)
+
+    {:reply, reply, state}
   end
 
-  defp log_event(tracker, pid, kind, value) do
-    log_event(tracker, pid, :unknown, kind, value)
+  defp log_event(%State{} = state, pid, kind, value) do
+    log_event(state, pid, :unknown, kind, value)
   end
 
-  defp log_event(tracker, pid, normalized_mfa, kind, value) do
-    Agent.get_and_update(tracker, fn %{} = state ->
-      with {:ok, entry} <- fetch_call(state, pid, normalized_mfa) do
-        entry = put_in(entry.events[kind], value)
-        result = result_after_log(pid, entry)
+  defp log_event(%State{} = state, pid, normalized_mfa, kind, value) do
+    with {:ok, entry} <- fetch_call(state, pid, normalized_mfa) do
+      entry = put_in(entry.events[kind], value)
+      result = result_after_log(pid, entry)
 
-        state =
-          case result do
-            {:ok, %Result{status: {:ready, _info}}} -> Map.delete(state, pid)
-            _other -> %{state | pid => entry}
-          end
+      state =
+        case result do
+          {:ok, %Result{status: {:ready, _info}}} ->
+            tracked_pids = Map.delete(state.tracked_pids, pid)
+            %State{state | tracked_pids: tracked_pids}
 
-        {result, state}
-      else
-        error -> {error, state}
-      end
-    end)
+          _other ->
+            put_in(state.tracked_pids[pid], entry)
+        end
+
+      {result, state}
+    else
+      error -> {error, state}
+    end
   end
 
-  defp fetch_call(%{} = state, pid, normalized_mfa) do
-    with %Entry{} = entry <- Map.get(state, pid, {:error, {:missing, pid, normalized_mfa}}),
+  defp fetch_call(%State{} = state, pid, normalized_mfa) do
+    with %Entry{} = entry <-
+           Map.get(state.tracked_pids, pid, {:error, {:missing, pid, normalized_mfa}}),
          :ok <- validate_expected_mfa(entry, pid, normalized_mfa) do
       {:ok, entry}
     end
