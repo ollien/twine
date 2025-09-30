7744c0495ae3e94d3b506e5ede2594984a94a698
Nick Krichevsky <nick@ollien.com>
2025-09-30 00:04:30 -0400

    Bump version to prepare for release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Twine.MixProject do
   def project do
     [
       app: :twine,
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: "~> 1.15",
       description: "Ergonomically trace calls in Elixir with recon_trace",
       package: package(),
